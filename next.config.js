a4a53eeab6893b203e0f5cecd539c7308c2c745b
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro.local>
2022-03-03 20:55:13 +0630

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["gogocdn.net"],
+  },
+  assetPrefix: "/animelux",
+  rewrites() {
+    return [{ source: "/animelux/_next/:path*", destination: "/_next/:path*" }];
   },
 };
 
