71f21f76ba32a3f4b814824101f812129b6d534d
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro.local>
2022-03-03 21:06:08 +0630

    update

--- pages/animelux/index.jsx
+++ pages/animelux/index.jsx
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-export default function Latest() {
+export default function Animelux() {
   const router = useRouter();
 
   useEffect(() => {
